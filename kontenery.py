edb462c581ef93cf5fd49da4292ac7a709a27e27
Karol Leśniewski <lesiulesniewski@gmail.com>
2022-12-19 14:46:55 +0100

    we made programs for searching colors

--- kontenery.py
+++ kontenery.py
@@ -1,3 +1,10 @@
-fruit = ["asdas", "dasada", 100]
-fruit.append("something")
-print(fruit[0])
+# 1
+"""stworzono kontener z kolorami"""
+colors = ["nebieski", "szary", "czerwony"]
+"""guess ma być zmienną do użytku w funkcji if """
+guess = input("zgadnij kolor ")
+"""stosujemy in, aby sprawdzić zwartość w tablicy """
+if guess in colors:
+    print("zgadłeś!")
+else: 
+    print("niestety :C, spórbuj jeszcze raz !")
